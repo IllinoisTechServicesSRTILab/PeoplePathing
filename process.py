fd1b17a607e5b2ab4b1f1f63d8bb2a0380af4e51
Abhinav Tekulapally <abhitekula@users.noreply.github.com>
2019-03-27 00:01:13 -0500

    Got person pathing to track people in video

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
-#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
 import boto3
 import json
@@ -16,7 +16,7 @@ class VideoDetect:
     bucket = ''
     video = ''
 
-    with open('configuration.json') as json_file:  
+    with open('configuration.json') as json_file:
         data = json.load(json_file)
         jobId = data['jobId']
         rek = boto3.client(data['rek'])
@@ -30,27 +30,26 @@ class VideoDetect:
 
         jobFound = False
         sqs = boto3.client('sqs')
-       
 
-        #=====================================
-        response = self.rek.start_label_detection(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
-                                         NotificationChannel={'RoleArn': self.roleArn, 'SNSTopicArn': self.topicArn})
-        #=====================================
+        # =====================================
+        response = self.rek.start_person_tracking(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
+                                                  NotificationChannel={'RoleArn': self.roleArn, 'SNSTopicArn': self.topicArn})
+        # =====================================
         print('Start Job Id: ' + response['JobId'])
-        dotLine=0
+        dotLine = 0
         while jobFound == False:
             sqsResponse = sqs.receive_message(QueueUrl=self.queueUrl, MessageAttributeNames=['ALL'],
-                                          MaxNumberOfMessages=10)
+                                              MaxNumberOfMessages=10)
 
             if sqsResponse:
-                
+
                 if 'Messages' not in sqsResponse:
-                    if dotLine<20:
+                    if dotLine < 20:
                         print('.', end='')
-                        dotLine=dotLine+1
+                        dotLine = dotLine+1
                     else:
                         print()
-                        dotLine=0    
+                        dotLine = 0
                     sys.stdout.flush()
                     continue
 
@@ -62,21 +61,20 @@ class VideoDetect:
                     if str(rekMessage['JobId']) == response['JobId']:
                         print('Matching Job Found:' + rekMessage['JobId'])
                         jobFound = True
-                        #=============================================
-                        self.GetResultsLabels(rekMessage['JobId'])
-                        #=============================================
+                        # =============================================
+                        self.GetResultsPersons(rekMessage['JobId'])
+                        # =============================================
 
                         sqs.delete_message(QueueUrl=self.queueUrl,
-                                       ReceiptHandle=message['ReceiptHandle'])
+                                           ReceiptHandle=message['ReceiptHandle'])
                     else:
                         print("Job didn't match:" +
                               str(rekMessage['JobId']) + ' : ' + str(response['JobId']))
                     # Delete the unknown message. Consider sending to dead letter queue
                     sqs.delete_message(QueueUrl=self.queueUrl,
-                                   ReceiptHandle=message['ReceiptHandle'])
+                                       ReceiptHandle=message['ReceiptHandle'])
 
         print('done')
-
 
     def GetResultsLabels(self, jobId):
         maxResults = 10
@@ -85,9 +83,9 @@ class VideoDetect:
 
         while finished == False:
             response = self.rek.get_label_detection(JobId=jobId,
-                                            MaxResults=maxResults,
-                                            NextToken=paginationToken,
-                                            SortBy='TIMESTAMP')
+                                                    MaxResults=maxResults,
+                                                    NextToken=paginationToken,
+                                                    SortBy='TIMESTAMP')
 
             print(response['VideoMetadata']['Codec'])
             print(str(response['VideoMetadata']['DurationMillis']))
@@ -95,33 +93,62 @@ class VideoDetect:
             print(response['VideoMetadata']['FrameRate'])
 
             for labelDetection in response['Labels']:
-                label=labelDetection['Label']
+                label = labelDetection['Label']
 
                 print("Timestamp: " + str(labelDetection['Timestamp']))
                 print("   Label: " + label['Name'])
-                print("   Confidence: " +  str(label['Confidence']))
+                print("   Confidence: " + str(label['Confidence']))
                 print("   Instances:")
                 for instance in label['Instances']:
-                    print ("      Confidence: " + str(instance['Confidence']))
-                    print ("      Bounding box")
-                    print ("        Top: " + str(instance['BoundingBox']['Top']))
-                    print ("        Left: " + str(instance['BoundingBox']['Left']))
-                    print ("        Width: " +  str(instance['BoundingBox']['Width']))
-                    print ("        Height: " +  str(instance['BoundingBox']['Height']))
+                    print("      Confidence: " + str(instance['Confidence']))
+                    print("      Bounding box")
+                    print("        Top: " +
+                          str(instance['BoundingBox']['Top']))
+                    print("        Left: " +
+                          str(instance['BoundingBox']['Left']))
+                    print("        Width: " +
+                          str(instance['BoundingBox']['Width']))
+                    print("        Height: " +
+                          str(instance['BoundingBox']['Height']))
                     print()
                 print()
-                print ("   Parents:")
+                print("   Parents:")
                 for parent in label['Parents']:
-                    print ("      " + parent['Name'])
-                print ()
+                    print("      " + parent['Name'])
+                print()
 
                 if 'NextToken' in response:
                     paginationToken = response['NextToken']
                 else:
                     finished = True
 
+    def GetResultsPersons(self, jobId):
+        maxResults = 10
+        paginationToken = ''
+        finished = False
+
+        while finished == False:
+            response = self.rek.get_person_tracking(JobId=jobId,
+                                                    MaxResults=maxResults,
+                                                    NextToken=paginationToken)
+
+            print(response['VideoMetadata']['Codec'])
+            print(str(response['VideoMetadata']['DurationMillis']))
+            print(response['VideoMetadata']['Format'])
+            print(response['VideoMetadata']['FrameRate'])
+
+            for personDetection in response['Persons']:
+                print('Index: ' + str(personDetection['Person']['Index']))
+                print('Timestamp: ' + str(personDetection['Timestamp']))
+                print()
+
+            if 'NextToken' in response:
+                paginationToken = response['NextToken']
+            else:
+                finished = True
+
 
 if __name__ == "__main__":
 
-    analyzer=VideoDetect()
+    analyzer = VideoDetect()
     analyzer.main()
